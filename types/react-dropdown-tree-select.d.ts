3b131002a9a52ee5527e6a618b78e63749ba4404
José Bezerra <jose.bezerra@omnipresent.com>
2024-06-04 14:42:17 -0300

    build(module): Module name changed prefixed with `@smartprocure`

--- types/react-dropdown-tree-select.d.ts
+++ types/react-dropdown-tree-select.d.ts
@@ -1,5 +1,5 @@
 // tslint:disable:interface-name
-declare module 'react-dropdown-tree-select' {
+declare module '@smartprocure/react-dropdown-tree-select' {
   import * as React from 'react'
 
   export type TreeData = Object | TreeNodeProps[]
